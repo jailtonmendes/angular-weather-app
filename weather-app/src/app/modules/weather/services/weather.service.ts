8fa74e818cbf8b12203166c2a966de40805f14b8
Jailton Mendes <jailtonmendes.silva@gmail.com>
2023-07-22 00:30:13 -0300

    Service creation

---
+++ weather-app/src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class WeatherService {
+
+  private apiKey = 'abcc401e263f27bd121da5dfbcb546a0';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {})
+  }
+
+}
